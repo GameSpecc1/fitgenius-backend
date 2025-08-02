ae5e0bab23eca36fde08673e21707db95f4275da
Brandon Bottomley <gamespecc@gmail.com>
2025-08-02 16:45:33 +0000

    Try fixing this error: Console Error: GenkitError: FAILED_PRECONDITION:

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,7 +1,7 @@
 import {genkit} from 'genkit';
-import {googleAI} from '@genkit-ai/googleai';
+import {vertexAI} from '@genkit-ai/googleai';
 
 export const ai = genkit({
-  plugins: [googleAI()],
+  plugins: [vertexAI({location: 'us-central1'})],
   model: 'googleai/gemini-2.0-flash',
 });
